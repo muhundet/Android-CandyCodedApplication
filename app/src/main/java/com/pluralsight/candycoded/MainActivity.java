d77a1ebf35fe2590481044de1ab35ff9b0a28ee6
muhundet <muhundetristone@gmail.com>
2020-09-28 10:25:41 +0200

    Task 1 - Show Store Information Activity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -80,14 +80,11 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main, menu);
         return true;
     }
-    // ***
-    // TODO - Task 1 - Show Store Information Activity
-    // ***
+
     @Override
     public boolean onOptionsItemSelected( MenuItem item) {
             Intent infoIntent = new Intent(this, InfoActivity.class);
             startActivity(infoIntent);
-
         return super.onOptionsItemSelected(item);
     }
 
